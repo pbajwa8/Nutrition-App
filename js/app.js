1c2e79525b3f97ba3c58af0840fc11d17e3d22dc
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-07-18 22:06:53 -0400

    bar calculator working

--- js/app.js
+++ js/app.js
@@ -14,38 +14,42 @@ app.controller('Controller', ['$scope', '$http', 'itemService', function($scope,
 
     init();
 
-
 		$scope.weight;
 		$scope.height;
 		$scope.age;
 		$scope.activityLevel;
+		$scope.calorieCount;
 
-		var convertedActivityLevel;
+		var finalBmr;
 
-		var activityLevelConverter = function(activityLevel) {
-			if (activityLevel === "sedentary") {
-				convertedActivityLevel = 1.2
-				return convertedActivityLevel
-			} else if (activityLevel === "light") {
-				convertedActivityLevel = 1.375
-				return convertedActivityLevel
-			} else if (activityLevel === "moderate") {
-				convertedActivityLevel = 1.55
-				return convertedActivityLevel
-			} else if (activityLevel === "hard") {
-				convertedActivityLevel = 1.725
-				return convertedActivityLevel
-			} else if (activityLevel === "very hard") {
-				convertedActivityLevel = 1.9
-				return convertedActivityLevel
+	$scope.onSubmit = function () {
+		var weight = angular.copy($scope.weight);
+		var height = angular.copy($scope.height);
+		var age = angular.copy($scope.age);
+		var activityLevel = angular.copy($scope.activityLevel);
+		bmrCalculator(weight, height, age, activityLevel);
+	}
+
+	function bmrCalculator (weight, height, age, activityLevel) {
+			if (activityLevel === "Sedentary") {
+				activityLevel = 1.2
+			} else if (activityLevel === "Light") {
+				activityLevel = 1.375
+			} else if (activityLevel === "Moderate") {
+				activityLevel = 1.55
+			} else if (activityLevel === "Hard") {
+				activityLevel = 1.725
+			} else if (activityLevel === "Very Hard") {
+				activityLevel = 1.9
 			}
-		};
+			var convertedWeight = 6.23 * $scope.weight;
+			var convertedHeight = 12.7 * $scope.height;
+			var convertedAge = 6.8 * $scope.age;
+			var bmr = 66 + convertedWeight + convertedHeight - convertedAge
+			finalBmr = bmr * activityLevel
+			$scope.calorieCount = Math.round(finalBmr)
+	};
 
-		var convertedWeight = 6.23 * $scope.weight;
-		var convertedHeight = 12.7 * $scope.height;
-		var convertedAge = 6.8 * $scope.age;
-		var bmr = 66 + convertedWeight + convertedHeight - convertedAge
 
-		$scope.calorieCount = bmr * convertedActivityLevel;
 
 }]);
