204f631f53e97240f8146063d59add47fa36f851
pbajwa8 <parambirb@Parambirs-MacBook-Pro.local>
2015-07-23 21:04:23 -0400

    wired up backend

--- js/app.js
+++ js/app.js
@@ -3,22 +3,6 @@ var app = angular.module('myApp', ['itemService']);
 
 app.controller('Controller', ['$scope', '$http', 'itemService', function($scope, $http, itemService) {
 
-    var items = {};
-    
-	function init(){
-        var svc = itemService;
-        svc.getItems().then(function(data, s){
-            $scope.data = data;
-            items = angular.copy($scope.data);
-            console.log(items);
-            return items
-        }, function(err,s){
-            console.log("fuck this shit")
-        });
-    }
-
-    init();
-
 		$scope.weight;
 		$scope.height;
 		$scope.age;
@@ -51,15 +35,21 @@ app.controller('Controller', ['$scope', '$http', 'itemService', function($scope,
 			var convertedHeight = 12.7 * $scope.height;
 			var convertedAge = 6.8 * $scope.age;
 			var bmr = 66 + convertedWeight + convertedHeight - convertedAge
-			finalBmr = bmr * activityLevel
-			$scope.calorieCount = Math.round(finalBmr)
-			getOptions();
+			finalBmr = Math.round(bmr * activityLevel)
+			$scope.calorieCount = finalBmr
+			getItems(finalBmr);
 	};
 
-	function getOptions () {
-		
-	};
 
+	function getItems(inputBMR){
+		var passer = inputBMR
+        var svc = itemService;
+        svc.getItems(passer).then(function(data, s){
+            $scope.data = data;
+        }, function(err,s){
+            console.log("fuck this shit")
+        });
+    }
 
 
 }]);
--- js/service.js
+++ js/service.js
@@ -1,39 +1,37 @@
 var app = angular.module('itemService', []);
 
 app.service("itemService", function($http, $q){
-    this.getItems = function(){
+    this.getItems = function(inputBMR){
         var deferred = $q.defer();
+        var specCalories = inputBMR;
         $http.get('https://api.myjson.com/bins/1n5yq').success(function(data, s){
-        	
-        	var specCalories = 1000;
-        	var returnArray = [];
+            var passedCalories = specCalories;
+        	var returnArray = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: [], 13: [], 14: [], 15: [], 16: [], 17: [], 18: [], 19: [], 20: []};
         	var breaker = 0
+
         	for (var i = 0; i < 1000; i++) {
-        	var random = Math.round(Math.random() * (200));
-        	var random2 = Math.round(Math.random() * (200));
-        	var random3 = Math.round(Math.random() * (200));
+	        	var random = Math.round(Math.floor(Math.random() * (69 - 0 + 1)));
+	        	var random2 = Math.round(Math.floor(Math.random() * (132 - 119 + 1)) + 119);
+	        	var random3 = Math.round(Math.floor(Math.random() * (220 - 132 + 1)) + 132);
 
-		    var mainsCalories = parseInt(data.collection1[random].calories);
-		    var mainsEntry = data.collection1[random].item;
-		    
-		    var sidesCalories = parseInt(data.collection1[random2].calories);
-		    var sidesEntry = data.collection1[random2].item;
-		    
-		    var drinksCalories = parseInt(data.collection1[random3].calories);
-		    var drinksEntry = data.collection1[random3].item;
-		    
-		    var totalCals = mainsCalories + sidesCalories + drinksCalories;
-		    
-		    if (totalCals <= specCalories) {
-		        returnArray.push(mainsEntry);
-		        breaker++;
-		        returnArray.push(sidesEntry);
-		        breaker++;
-		        returnArray.push(drinksEntry);
-		        breaker++;
-		    	}
-		    if (breaker === 3) { break; }
-
+			    var mainsCalories = parseInt(data.collection1[random].calories);
+			    var mainsEntry = data.collection1[random].item;
+			    
+			    var sidesCalories = parseInt(data.collection1[random2].calories);
+			    var sidesEntry = data.collection1[random2].item;
+			    
+			    var drinksCalories = parseInt(data.collection1[random3].calories);
+			    var drinksEntry = data.collection1[random3].item;
+			    
+			    var totalCals = mainsCalories + sidesCalories + drinksCalories;
+			    
+			    if (totalCals <= passedCalories) {
+			        returnArray[breaker].push(mainsEntry);
+			        returnArray[breaker].push(sidesEntry);
+			        returnArray[breaker].push(drinksEntry);
+			        breaker++;
+			    	}
+			    if (breaker === 21) { break; }
 			};
 
             deferred.resolve(returnArray);
